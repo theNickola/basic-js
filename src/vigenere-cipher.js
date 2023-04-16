4cd418cfe8037c81889020956de52cc44fddb28c
theNickola <kurilchik.nv@gmail.com>
2023-04-17 00:33:34 +0300

    feat: vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+/*
+  constructor(type = true) {
+      this.type = type;
+  }*/
+
+
+  encrypt(w,k) {throw new NotImplementedError('Not implemented');
+    /*
+    let directCrypt = this.getCrypt(w,k,'encrypt');
+    return this.type ? directCrypt : directCrypt.split('').reverse().join('');*/
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+
+  decrypt(w,k) {throw new NotImplementedError('Not implemented');
+    /*
+    let directCrypt = this.getCrypt(w,k,'decrypt');
+    return this.type ? directCrypt : directCrypt.split('').reverse().join('');*/
+  }
+
+
+  getCrypt(w, k, typeCrypt) {
+    if (w === undefined || k === undefined) throw new Error("Incorrect arguments!");
+    function insertSym(str, sym, pos) {
+      let array = str.split('');
+      array.splice(pos, 0, sym);
+      return array.join('');
+    }
+    function deleteSym(str, pos) {
+      let array = str.split('');
+      array.splice(pos, 1);
+      return array.join('');
+    }
+    
+    let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let countRepeat = Math.ceil(w.length / k.length);
+    k= k.repeat(countRepeat).slice(0,w.length);
+    w = w.toUpperCase();
+    k = k.toUpperCase();
+    let result = '';
+    let wOrign = w;
+  
+    for(let i=0; i<w.length; i++) {
+      if(!abc.includes(w[i])){
+        w = deleteSym(w,i);
+        i--;
+      }
+    }
+  
+    if(typeCrypt === 'encrypt') {
+      for(let i=0; i<w.length; i++) {
+        let indexEncrSym = abc.indexOf(k[i]) + abc.indexOf(w[i]);
+        if (indexEncrSym > (abc.length - 1))
+          indexEncrSym -= abc.length;
+        result += abc[indexEncrSym];
+      }
+    }
+    else if(typeCrypt === 'decrypt') {
+      for(let i=0; i<w.length; i++) {
+        let indexEncrSym = abc.indexOf(w[i]) - abc.indexOf(k[i]);
+        if (indexEncrSym < 0)
+          indexEncrSym += (abc.length);
+        result += abc[indexEncrSym];
+      }
+    }
+  
+    for(let i=0; i<wOrign.length; i++) {
+      if(!abc.includes(wOrign[i]))
+        result = insertSym(result,wOrign[i],i);
+    }
+    return result;
   }
 }
 
